b6aed9d691190d66841b4bdd7fc42655cec79b7a
AlfredYang1986 <yangyuanpig@gmail.com>
2019-09-25 16:40:42 +0800

    schema 管理

--- src/main/scala/com/pharbers/StreamEngine/main.scala
+++ src/main/scala/com/pharbers/StreamEngine/main.scala
@@ -1,10 +1,16 @@
 
 import java.util.UUID
 
-import org.apache.spark.{SparkConf}
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.types._
+import org.apache.avro.generic.GenericDatumReader
+import org.apache.avro.generic.GenericRecord
+import org.apache.avro.io.DecoderFactory
+import com.pharbers.StreamEngine.schemaReg.SchemaReg
+import org.apache.avro.Schema
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.{SparkSession}
 import org.apache.spark.sql.functions.from_json
+import org.apache.spark.sql.functions.udf
 
 object main extends App {
     val yarnJars: String = "hdfs://spark.master:9000/jars/sparkJars"
@@ -18,22 +24,18 @@ object main extends App {
         .set("spark.executor.memory", "1g")
         .set("spark.executor.cores", "1")
         .set("spark.executor.instances", "1")
-
-    val schema: StructType = StructType(Seq(
-        StructField("Time", StringType),
-        StructField("Hostname", StringType),
-        StructField("ProjectName",StringType),
-        StructField("File", StringType),
-        StructField("Func", StringType),
-        StructField("JobId", StringType),
-        StructField("TraceId", StringType),
-        StructField("UserId", StringType),
-        StructField("Message", StringType),
-        StructField("Level", StringType)
-    ))
+        .set("spark.sql.avro.compression.codec", "deflate")
+        .set("spark.sql.avro.deflate.level", "5")
 
     val spark = SparkSession.builder()
         .config(conf).getOrCreate()
+
+    spark.sqlContext.sparkContext.hadoopConfiguration.set("avro.mapred.ignore.inputs.without.extension", "true")
+
+    spark.sparkContext.addFile("./kafka.broker1.keystore.jks")
+    spark.sparkContext.addFile("./kafka.broker1.truststore.jks")
+    spark.sparkContext.addJar("./jars/spark-avro_2.11-2.4.4.jar")
+    spark.sparkContext.addJar("./target/BP-Stream-Engine-1.0-SNAPSHOT.jar")
 
     import spark.implicits._
 
@@ -46,24 +48,51 @@ object main extends App {
         .option("kafka.ssl.truststore.location", "./kafka.broker1.truststore.jks")
         .option("kafka.ssl.truststore.password", "pharbers")
         .option("kafka.ssl.endpoint.identification.algorithm", " ")
-        .option("subscribe", "test001")
-        .option("startingOffsets", "earliest")
+//        .option("subscribe", "test001")
+        .option("subscribe", "oss_source_1")
+        .option("startingOffsets", """{"oss_source_1":{"0":-2}}""")
         .load()
 
-    val selectDf = logsDf.selectExpr(
+    val selectDf = logsDf
+        .selectExpr(
         "CAST(key AS STRING)",
-        "CAST(value AS STRING)",
-        "timestamp").as[(String, String, String)].toDF()
-        .withWatermark("timestamp", "24 hours")
-        .select(
-            from_json($"value", schema).as("data")
-        ).select("data.*")
+//        "CAST(value AS STRING)",
+        "value",
+        "timestamp").toDF()
+        .withWatermark("timestamp", "24 hours").select("value")
+//        .as[Array[Byte]]
+//        .as[String]
+
+    val decodeAvro = udf { x: Array[Byte] =>
+        println("=======>" + x)
+        val schema = Schema.parse(SchemaReg.tmp)
+        println("=======>" + schema)
+        val reader1 = new GenericDatumReader[GenericRecord](schema)
+
+        val decoder1 = DecoderFactory.get.binaryDecoder(x, null)
+        val result = reader1.read(null, decoder1)
+        println("=======>" + result)
+        println("=======>" + result.toString)
+        result.toString
+    }
+
+    val decodeDf = selectDf.withColumn("value", decodeAvro('value))
+
+    val result = decodeDf.toDF().select(
+        from_json($"value", SchemaReg.baseSchema).as("data")
+    ).select("data.*")
+
+    val sch = result.printSchema()
 
     val jobId = UUID.randomUUID()
-    val query = selectDf.writeStream
+    val path = "hdfs://192.168.100.137:9000/test/streaming/" + jobId + "/files"
+
+    val query = decodeDf.writeStream
          .outputMode("append")
+//        .outputMode("complete")
 //         .format("console")
-        .format("parquet")
+//        .option("truncate", false)
+        .format("csv")
         .option("checkpointLocation", "hdfs://192.168.100.137:9000/test/streaming/" + jobId + "/checkpoint")
         .option("path", "hdfs://192.168.100.137:9000/test/streaming/" + jobId + "/files")
         .start()
