29393e699060c3578978400ca0a60898cc94173f
dcs <419787760@qq.com>
2019-09-25 17:03:14 +0800

    jar all

--- src/main/scala/com/pharbers/StreamEngine/main.scala
+++ src/main/scala/com/pharbers/StreamEngine/main.scala
@@ -42,18 +42,17 @@ object main extends App {
     import spark.implicits._
 
     private val topic = "oss_source_1"
-    private val kafkaUrl = "http://123.56.179.133:9092"
+
     private val schemaRegistryUrl = "http://123.56.179.133:8081"
 
     private val schemaRegistryClient = new CachedSchemaRegistryClient(schemaRegistryUrl, 128)
-    private val kafkaAvroDeserializer = new AvroDeserializer(schemaRegistryClient)
 
     private val avroSchema = schemaRegistryClient.getLatestSchemaMetadata(topic + "-value").getSchema
-    private var sparkSchema = SchemaConverters.toSqlType(new Schema.Parser().parse(avroSchema))
+    private val sparkSchema = SchemaConverters.toSqlType(new Schema.Parser().parse(avroSchema))
 
     spark.sparkContext.addFile("kafka.broker1.keystore.jks")
     spark.sparkContext.addFile("kafka.broker1.truststore.jks")
-    spark.sparkContext.addJar("target/jar-all.jar")
+//    spark.sparkContext.addJar("target/jar-all.jar")
 
     spark.udf.register("deserialize", (bytes: Array[Byte]) =>
         DeserializerWrapper.deserializer.deserialize(bytes)
