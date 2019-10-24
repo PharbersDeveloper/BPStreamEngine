0e399be2ee3acb4c38b38181c31dc4cc45247a79
Faiz <891366402@qq.com>
2019-10-24 19:28:08 +0800

    抽取Parquet形成SampleData，抽取MetaData形成Schema，并发送邮件
    该版本为演示效果提供，内部读取未使用流式，路径基本写死，不适用与版本合并更新，周六日进行代码重构，实现流式读取，Kafka策略初始化，监听处理Job任务

--- src/main/scala/com/pharbers/StreamEngine/Jobs/SandBoxJob/SandBoxSampleData/BPSSandBoxSampleDataJob.scala
+++ src/main/scala/com/pharbers/StreamEngine/Jobs/SandBoxJob/SandBoxSampleData/BPSSandBoxSampleDataJob.scala
@@ -1,5 +1,6 @@
 package com.pharbers.StreamEngine.Jobs.SandBoxJob.SandBoxSampleData
 
+import com.pharbers.StreamEngine.Jobs.SandBoxJob.FileMeta2Mongo.BPSMongo.BPFileMeta2Mongo
 import com.pharbers.StreamEngine.Utils.StreamJob.JobStrategy.BPSJobStrategy
 import com.pharbers.StreamEngine.Utils.StreamJob.{BPSJobContainer, BPStreamJob}
 import org.apache.spark.sql
@@ -10,28 +11,29 @@ object BPSSandBoxSampleDataJob {
 	          spark: SparkSession,
 	          inputStream: Option[sql.DataFrame],
 	          container: BPSJobContainer): BPSSandBoxSampleDataJob =
-		new BPSSandBoxSampleDataJob(id, spark, container)
+		new BPSSandBoxSampleDataJob(id, spark, inputStream, container)
 }
 
 class BPSSandBoxSampleDataJob(val id: String,
                               val spark: SparkSession,
-//                              val is: Option[sql.DataFrame],
+                              val is: Option[sql.DataFrame],
                               val container: BPSJobContainer) extends BPStreamJob {
 	type T = BPSJobStrategy
 	override val strategy = null
-	import spark.implicits._
 	
 	override def exec(): Unit = {
-//		inputStream = is
+		inputStream = is
 		inputStream match {
 			case None =>
 			case Some(is) =>
-				is.show(false)
-//				val traceIds = is.selectExpr("traceId")
-//				val traceId = traceIds.first().getAs[String]("traceId")
-//				val sampleData = is.selectExpr("data").toJSON.take(5).toList
-//				BPFileMeta2Mongo(traceId, sampleData, "", 0).MetaData()
-		
+				println("SampleData =>>>>>>>>>>>>>>>>>>>>>>")
+				val traceIds = is.selectExpr("traceId")
+				val traceId = traceIds.first().getAs[String]("traceId")
+				val sampleData = is.selectExpr("data")
+					.take(5)
+					.map(x => x.getAs[String]("data").replaceAll("""\\"""", ""))
+					.toList
+				BPFileMeta2Mongo(traceId, sampleData, "", 0).SampleData()
 		}
 	}
 	
