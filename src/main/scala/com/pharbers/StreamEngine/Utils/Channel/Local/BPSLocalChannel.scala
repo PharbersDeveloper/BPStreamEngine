a9389cc0323e9bd305bb7b3e5861cae238b00eae
clock <2567729201@qq.com>
2019-11-08 16:26:10 +0800

    Channel 注释

--- src/main/scala/com/pharbers/StreamEngine/Utils/Channel/Local/BPSLocalChannel.scala
+++ src/main/scala/com/pharbers/StreamEngine/Utils/Channel/Local/BPSLocalChannel.scala
@@ -3,6 +3,7 @@ package com.pharbers.StreamEngine.Utils.Channel.Local
 import com.pharbers.StreamEngine.Utils.Annotation.Component
 import com.pharbers.StreamEngine.Utils.Event.StreamListener.BPStreamListener
 import com.pharbers.StreamEngine.Utils.ThreadExecutor.ThreadExecutor
+import com.pharbers.util.log.PhLogable
 
 object BPSLocalChannel {
     var channel: Option[BPSLocalChannel] = None
@@ -27,16 +28,19 @@ object BPSLocalChannel {
     }
 }
 
+// TODO 希望可以补全注释，因为我不知道这是干什么的
 @Component(name = "BPSLocalChannel", `type` = "BPSLocalChannel")
-class BPSLocalChannel extends Runnable {
+class BPSLocalChannel extends Runnable with PhLogable {
 
     var lst: List[BPStreamListener] = Nil
 
     def registerListener(listener: BPStreamListener): Unit = lst = listener :: lst
+
     def trigger(): Unit = lst.foreach(_.trigger(null))
 
     override def run(): Unit = {
-        while(true) {
+        logger.info("Local Channel Server")
+        while (true) {
             trigger()
             Thread.sleep(1000)
         }
