61705d0fed80fa640b3a31f60b2059c92a9d3432
Unknown <jeorch@foxmail.com>
2019-12-17 19:46:51 +0800

    更新sql

--- src/test/scala/com/pharbers/StreamEngine/Jobs/Hive2EsJob/StartHive2EsJobTest.scala
+++ src/test/scala/com/pharbers/StreamEngine/Jobs/Hive2EsJob/StartHive2EsJobTest.scala
@@ -14,7 +14,9 @@ class StartHive2EsJobTest extends FunSuite{
         implicit val formats: DefaultFormats.type = DefaultFormats
         val jobId = UUID.randomUUID().toString
 
-        val sql = "SELECT * FROM cpa WHERE ( YEAR like '2018%' AND MONTH >= 8 ) OR ( YEAR like '2019%' AND MONTH <= 8 )"
+        val sql = "SELECT COMPANY, SOURCE, PROVINCE_NAME, CITY_NAME, HOSP_NAME, HOSP_CODE, HOSP_LEVEL, MOLE_NAME, PRODUCT_NAME, MANUFACTURER_NAME, MKT," +
+            " CAST(SALES_VALUE As DOUBLE) AS SALES, CAST(YEAR As INT) AS YEAR, CAST(MONTH As INT) AS MONTH" +
+            " FROM cpa WHERE ( YEAR like '2018%' AND MONTH >= 8 ) OR ( YEAR like '2019%' AND MONTH <= 8 )"
         val indexName = "cpa"
         val topic = "Hive2EsJobSubmit"
 
