9e13bad687ea86f8c7ce2c63108c875dbf2cd837
Unknown <jeorch@foxmail.com>
2019-12-25 13:55:28 +0800

    各维度数据聚合完成

--- src/test/scala/com/pharbers/StreamEngine/Utils/Session/Spark/BPSparkSessionTest.scala
+++ src/test/scala/com/pharbers/StreamEngine/Utils/Session/Spark/BPSparkSessionTest.scala
@@ -55,82 +55,80 @@ class BPSparkSessionTest extends FunSuite with PhLogable {
         val formatDF = data.selectExpr(keys: _*)
             .withColumn("YEAR", col("YEAR").cast(DataTypes.IntegerType))
             .withColumn("MONTH", col("MONTH").cast(DataTypes.IntegerType))
-            .withColumn("YM", col("YEAR").*(100).+(col("MONTH")))
+            .withColumn("YM", col("YEAR") * 100 + col("MONTH") )
             .withColumn("SALES_QTY", col("SALES_QTY").cast(DataTypes.LongType))
             .withColumn("SALES_VALUE", col("SALES_VALUE").cast(DataTypes.DoubleType))
 
-        //缩小数据范围，需求中最小维度是分子，先计算出分子级别在单个年月、省&城市级别、产品维度的聚合数据
-        val moleLevelDF = formatDF.groupBy("YM", "PROVINCE_NAME", "CITY_NAME", "PRODUCT_NAME", "MOLE_NAME")
+        //缩小数据范围，需求中最小维度是分子，先计算出分子级别在单个公司年月市场、省&城市级别、产品&分子维度的聚合数据
+        val moleLevelDF = formatDF.groupBy("COMPANY", "YM", "MKT", "PROVINCE_NAME", "CITY_NAME", "PRODUCT_NAME", "MOLE_NAME")
             .agg(expr("SUM(SALES_QTY) as MOLE_SALES_QTY"), expr("SUM(SALES_VALUE) as MOLE_SALES_VALUE"))
 
         //TODO:得保证数据源中包含两年的数据
         val current2YearYmList = moleLevelDF.select("YM").distinct().sort("YM").collect().map(_(0)).toList.takeRight(24).asInstanceOf[List[Int]]
 
         val moleWindow = Window
-            .partitionBy("YM", "PROVINCE_NAME", "CITY_NAME", "PRODUCT_NAME", "MOLE_NAME")
-
-        val lastMonthMoleWindow = moleWindow
-            .orderBy(to_date(col("YM").cast("string"), "yyyyMM").cast("timestamp").cast("long"))
-            .rangeBetween(-86400 * 31, Window.currentRow)
+            .partitionBy("COMPANY", "MKT", "PROVINCE_NAME", "CITY_NAME", "PRODUCT_NAME", "MOLE_NAME")
 
         val prodWindow = Window
-            .partitionBy("YM", "PROVINCE_NAME", "CITY_NAME", "PRODUCT_NAME")
+            .partitionBy("COMPANY", "MKT", "PROVINCE_NAME", "CITY_NAME", "PRODUCT_NAME")
 
         val cityWindow = Window
-            .partitionBy("YM", "PROVINCE_NAME", "CITY_NAME")
+            .partitionBy("COMPANY", "MKT", "PROVINCE_NAME", "CITY_NAME")
 
         val provWindow = Window
-            .partitionBy("YM", "PROVINCE_NAME")
+            .partitionBy("COMPANY", "MKT", "PROVINCE_NAME")
 
-        val ymWindow = Window
-            .partitionBy("YM")
+        val mktWindow = Window
+            .partitionBy("COMPANY", "MKT")
 
         val current2YearDF = moleLevelDF
             .filter(col("YM") >= current2YearYmList.min.toString.toInt && col("YM") <= current2YearYmList.max.toString.toInt)
-//            .withColumn("PROD_SALES_QTY", sum("MOLE_SALES_QTY").over(prodWindow))
-//            .withColumn("PROD_SALES_VALUE", sum("MOLE_SALES_VALUE").over(prodWindow))
-//            .withColumn("CITY_SALES_QTY", sum("MOLE_SALES_QTY").over(cityWindow))
-//            .withColumn("CITY_SALES_VALUE", sum("MOLE_SALES_VALUE").over(cityWindow))
-//            .withColumn("PROV_SALES_QTY", sum("MOLE_SALES_QTY").over(provWindow))
-//            .withColumn("PROV_SALES_VALUE", sum("MOLE_SALES_VALUE").over(provWindow))
-//            .withColumn("YM_SALES_QTY", sum("MOLE_SALES_QTY").over(ymWindow))
-//            .withColumn("YM_SALES_VALUE", sum("MOLE_SALES_VALUE").over(ymWindow))
+            .withColumn("PROD_SALES_QTY", sum("MOLE_SALES_QTY").over(currMonthWindow(prodWindow)))
+            .withColumn("PROD_SALES_VALUE", sum("MOLE_SALES_VALUE").over(currMonthWindow(prodWindow)))
+            .withColumn("PROD_SALES_VALUE_RANK", dense_rank.over(prodWindow.orderBy("PROD_SALES_VALUE")))
+            .withColumn("CITY_SALES_QTY", sum("MOLE_SALES_QTY").over(currMonthWindow(cityWindow)))
+            .withColumn("CITY_SALES_VALUE", sum("MOLE_SALES_VALUE").over(currMonthWindow(cityWindow)))
+            .withColumn("PROV_SALES_QTY", sum("MOLE_SALES_QTY").over(currMonthWindow(provWindow)))
+            .withColumn("PROV_SALES_VALUE", sum("MOLE_SALES_VALUE").over(currMonthWindow(provWindow)))
+            .withColumn("MKT_SALES_QTY", sum("MOLE_SALES_QTY").over(currMonthWindow(mktWindow)))
+            .withColumn("MKT_SALES_VALUE", sum("MOLE_SALES_VALUE").over(currMonthWindow(mktWindow)))
             //fill lastMonth values
-            .withColumn("LAST_M_MOLE_SALES_QTY", sum("MOLE_SALES_QTY").over(lastMonthMoleWindow) - col("MOLE_SALES_QTY"))
-            .withColumn("LAST_M_MOLE_SALES_VALUE", sum("MOLE_SALES_VALUE").over(lastMonthMoleWindow) - col("MOLE_SALES_VALUE"))
-            .na.fill(Map("LAST_M_MOLE_SALES_QTY" -> 0, "LAST_M_MOLE_SALES_VALUE" -> 0.0))
-//            .withColumn("LAST_M_PROD_SALES_QTY", first("PROD_SALES_QTY").over(lastMonthWindow(prodWindow)))
-//            .withColumn("LAST_M_PROD_SALES_VALUE", first("PROD_SALES_VALUE").over(lastMonthWindow(prodWindow)))
-//            .na.fill(Map("LAST_M_PROD_SALES_QTY" -> 0, "LAST_M_PROD_SALES_VALUE" -> 0.0))
-//            .withColumn("LAST_M_CITY_SALES_QTY", first("CITY_SALES_QTY").over(lastMonthWindow(cityWindow)))
-//            .withColumn("LAST_M_CITY_SALES_VALUE", first("CITY_SALES_VALUE").over(lastMonthWindow(cityWindow)))
-//            .na.fill(Map("LAST_M_CITY_SALES_QTY" -> 0, "LAST_M_CITY_SALES_VALUE" -> 0.0))
-//            .withColumn("LAST_M_PROV_SALES_QTY", first("PROV_SALES_QTY").over(lastMonthWindow(provWindow)))
-//            .withColumn("LAST_M_PROV_SALES_VALUE", first("PROV_SALES_VALUE").over(lastMonthWindow(provWindow)))
-//            .na.fill(Map("LAST_M_PROV_SALES_QTY" -> 0, "LAST_M_PROV_SALES_VALUE" -> 0.0))
-//            .withColumn("LAST_M_YM_SALES_QTY", first("YM_SALES_QTY").over(lastMonthWindow(ymWindow)))
-//            .withColumn("LAST_M_YM_SALES_VALUE", first("YM_SALES_VALUE").over(lastMonthWindow(ymWindow)))
-//            .na.fill(Map("LAST_M_YM_SALES_QTY" -> 0, "LAST_M_YM_SALES_VALUE" -> 0.0))
-//            //fill lastYear values
-//            .withColumn("LAST_Y_MOLE_SALES_QTY", first("MOLE_SALES_QTY").over(lastYearWindow(moleWindow)))
-//            .withColumn("LAST_Y_MOLE_SALES_VALUE", first("MOLE_SALES_VALUE").over(lastYearWindow(moleWindow)))
-//            .na.fill(Map("LAST_Y_MOLE_SALES_QTY" -> 0, "LAST_Y_MOLE_SALES_VALUE" -> 0.0))
-//            .withColumn("LAST_Y_PROD_SALES_QTY", first("PROD_SALES_QTY").over(lastYearWindow(prodWindow)))
-//            .withColumn("LAST_Y_PROD_SALES_VALUE", first("PROD_SALES_VALUE").over(lastYearWindow(prodWindow)))
-//            .na.fill(Map("LAST_Y_PROD_SALES_QTY" -> 0, "LAST_Y_PROD_SALES_VALUE" -> 0.0))
-//            .withColumn("LAST_Y_CITY_SALES_QTY", first("CITY_SALES_QTY").over(lastYearWindow(cityWindow)))
-//            .withColumn("LAST_Y_CITY_SALES_VALUE", first("CITY_SALES_VALUE").over(lastYearWindow(cityWindow)))
-//            .na.fill(Map("LAST_Y_CITY_SALES_QTY" -> 0, "LAST_Y_CITY_SALES_VALUE" -> 0.0))
-//            .withColumn("LAST_Y_PROV_SALES_QTY", first("PROV_SALES_QTY").over(lastYearWindow(provWindow)))
-//            .withColumn("LAST_Y_PROV_SALES_VALUE", first("PROV_SALES_VALUE").over(lastYearWindow(provWindow)))
-//            .na.fill(Map("LAST_Y_PROV_SALES_QTY" -> 0, "LAST_Y_PROV_SALES_VALUE" -> 0.0))
-//            .withColumn("LAST_Y_YM_SALES_QTY", first("YM_SALES_QTY").over(lastYearWindow(ymWindow)))
-//            .withColumn("LAST_Y_YM_SALES_VALUE", first("YM_SALES_VALUE").over(lastYearWindow(ymWindow)))
-//            .na.fill(Map("LAST_Y_YM_SALES_QTY" -> 0, "LAST_Y_YM_SALES_VALUE" -> 0.0))
+            .withColumn("LAST_M_PROD_SALES_QTY", first("PROD_SALES_QTY").over(lastMonthWindow(prodWindow)))
+            .withColumn("LAST_M_PROD_SALES_VALUE", first("PROD_SALES_VALUE").over(lastMonthWindow(prodWindow)))
+            .na.fill(Map("LAST_M_PROD_SALES_QTY" -> 0, "LAST_M_PROD_SALES_VALUE" -> 0.0))
+            .withColumn("LAST_M_CITY_SALES_QTY", first("CITY_SALES_QTY").over(lastMonthWindow(cityWindow)))
+            .withColumn("LAST_M_CITY_SALES_VALUE", first("CITY_SALES_VALUE").over(lastMonthWindow(cityWindow)))
+            .na.fill(Map("LAST_M_CITY_SALES_QTY" -> 0, "LAST_M_CITY_SALES_VALUE" -> 0.0))
+            .withColumn("LAST_M_PROV_SALES_QTY", first("PROV_SALES_QTY").over(lastMonthWindow(provWindow)))
+            .withColumn("LAST_M_PROV_SALES_VALUE", first("PROV_SALES_VALUE").over(lastMonthWindow(provWindow)))
+            .na.fill(Map("LAST_M_PROV_SALES_QTY" -> 0, "LAST_M_PROV_SALES_VALUE" -> 0.0))
+            .withColumn("LAST_M_MKT_SALES_QTY", first("MKT_SALES_QTY").over(lastMonthWindow(mktWindow)))
+            .withColumn("LAST_M_MKT_SALES_VALUE", first("MKT_SALES_VALUE").over(lastMonthWindow(mktWindow)))
+            .na.fill(Map("LAST_M_MKT_SALES_QTY" -> 0, "LAST_M_MKT_SALES_VALUE" -> 0.0))
+            //fill lastYear values
+            .withColumn("LAST_Y_MOLE_SALES_QTY", first("MOLE_SALES_QTY").over(lastYearWindow(moleWindow)))
+            .withColumn("LAST_Y_MOLE_SALES_VALUE", first("MOLE_SALES_VALUE").over(lastYearWindow(moleWindow)))
+            .na.fill(Map("LAST_Y_MOLE_SALES_QTY" -> 0, "LAST_Y_MOLE_SALES_VALUE" -> 0.0))
+            .withColumn("LAST_Y_PROD_SALES_QTY", first("PROD_SALES_QTY").over(lastYearWindow(prodWindow)))
+            .withColumn("LAST_Y_PROD_SALES_VALUE", first("PROD_SALES_VALUE").over(lastYearWindow(prodWindow)))
+            .na.fill(Map("LAST_Y_PROD_SALES_QTY" -> 0, "LAST_Y_PROD_SALES_VALUE" -> 0.0))
+            .withColumn("LAST_Y_CITY_SALES_QTY", first("CITY_SALES_QTY").over(lastYearWindow(cityWindow)))
+            .withColumn("LAST_Y_CITY_SALES_VALUE", first("CITY_SALES_VALUE").over(lastYearWindow(cityWindow)))
+            .na.fill(Map("LAST_Y_CITY_SALES_QTY" -> 0, "LAST_Y_CITY_SALES_VALUE" -> 0.0))
+            .withColumn("LAST_Y_PROV_SALES_QTY", first("PROV_SALES_QTY").over(lastYearWindow(provWindow)))
+            .withColumn("LAST_Y_PROV_SALES_VALUE", first("PROV_SALES_VALUE").over(lastYearWindow(provWindow)))
+            .na.fill(Map("LAST_Y_PROV_SALES_QTY" -> 0, "LAST_Y_PROV_SALES_VALUE" -> 0.0))
+            .withColumn("LAST_Y_MKT_SALES_QTY", first("MKT_SALES_QTY").over(lastYearWindow(mktWindow)))
+            .withColumn("LAST_Y_MKT_SALES_VALUE", first("MKT_SALES_VALUE").over(lastYearWindow(mktWindow)))
+            .na.fill(Map("LAST_Y_MKT_SALES_QTY" -> 0, "LAST_Y_MKT_SALES_VALUE" -> 0.0))
 
         current2YearDF.show(20)
 //        current2YearDF.filter(col("PRODUCT_NAME") === "丽泉" && col("CITY_NAME") === "七台河市" && col("YM") === "201803").show(10)
         return current2YearDF
+    }
+
+    def currMonthWindow(window: WindowSpec): WindowSpec = {
+        window.orderBy(col("YM").cast(DataTypes.IntegerType)).rangeBetween(0, 0)
     }
 
     def lastMonthWindow(window: WindowSpec): WindowSpec = {
