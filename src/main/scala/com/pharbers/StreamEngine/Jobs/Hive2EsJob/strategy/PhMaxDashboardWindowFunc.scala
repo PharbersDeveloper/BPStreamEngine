1329807b707337161c414cb221e6c51047cadb74
Unknown <jeorch@foxmail.com>
2020-01-17 19:14:53 +0800

    按产品需求调整max-result聚合数据

--- src/main/scala/com/pharbers/StreamEngine/Jobs/Hive2EsJob/strategy/PhMaxDashboardWindowFunc.scala
+++ src/main/scala/com/pharbers/StreamEngine/Jobs/Hive2EsJob/strategy/PhMaxDashboardWindowFunc.scala
@@ -21,11 +21,21 @@ case class PhMaxDashboardWindowFunc(df: DataFrame) extends PhLogable {
     private def lastYearWindow(window: WindowSpec): WindowSpec =
         window.orderBy(col("DATE").cast(DataTypes.IntegerType)).rangeBetween(-100, -100)
 
-    def GenerateSalesRowWith(time: String, prodLevel: String, region: String): PhMaxDashboardWindowFunc = {
+    def GenerateSalesAndRankRowWith(time: String, prodLevel: String, region: String): PhMaxDashboardWindowFunc = {
 
-        val prodLevelKeys: List[String] = prodLevel match {
+        val prodSalesKeys: List[String] = prodLevel match {
             case "MOLE" => "PRODUCT_NAME" :: "MOLE_NAME" :: Nil
             case "PROD" => "PRODUCT_NAME" :: Nil
+            case "MKT" => "MKT" :: Nil
+            case "TOTAL" => Nil
+            case _ => logger.error(s"${prodLevel} not implemented."); Nil
+        }
+
+        val prodRankKeys: List[String] = prodLevel match {
+            case "MOLE" => "PRODUCT_NAME" :: Nil
+            case "PROD" => Nil
+            case "MKT" => Nil
+            case "TOTAL" => Nil
             case _ => logger.error(s"${prodLevel} not implemented."); Nil
         }
 
@@ -33,43 +43,32 @@ case class PhMaxDashboardWindowFunc(df: DataFrame) extends PhLogable {
             case "CITY" => "PROVINCE" :: "CITY" :: Nil
             case "PROV" => "PROVINCE" :: Nil
             case "NATION" => Nil
-            case _ => logger.error(s"${prodLevel} not implemented."); Nil
+            case _ => logger.error(s"${region} not implemented."); Nil
         }
 
-        val mktWindow: WindowSpec = Window.partitionBy("MKT", prodLevelKeys ::: regionKeys: _*)
+        val mktWindow: WindowSpec = Window.partitionBy("COMPANY", prodSalesKeys ::: regionKeys: _*)
+        val rankWindow: WindowSpec = Window.partitionBy("DATE", prodRankKeys ::: regionKeys: _*)
 
         val newDF: DataFrame = time match {
             case "CURR" => df
                 .withColumn(time + "_" + prodLevel + "_SALES_IN_" + region, sum("MOLE_SALES_VALUE").over(currMonthWindow(mktWindow)))
+                .withColumn(time + "_" + prodLevel + "_RANK_IN_" + region, dense_rank.over(rankWindow.orderBy(col(time + "_" + prodLevel + "_SALES_IN_" + region).desc)))
             case "LAST_M" => df
                 .withColumn(time + "_" + prodLevel + "_SALES_IN_" + region, sum("MOLE_SALES_VALUE").over(lastMonthWindow(mktWindow)))
+                .withColumn(time + "_" + prodLevel + "_RANK_IN_" + region, dense_rank.over(rankWindow.orderBy(col(time + "_" + prodLevel + "_SALES_IN_" + region).desc)))
             case "LAST_Y" => df
                 .withColumn(time + "_" + prodLevel + "_SALES_IN_" + region, sum("MOLE_SALES_VALUE").over(lastYearWindow(mktWindow)))
+                .withColumn(time + "_" + prodLevel + "_RANK_IN_" + region, dense_rank.over(rankWindow.orderBy(col(time + "_" + prodLevel + "_SALES_IN_" + region).desc)))
             case _ => logger.error(s"${time} not implemented."); df
         }
 
         PhMaxDashboardWindowFunc(newDF)
     }
 
-    def GenerateRankAndShareRowWith(time: String, prodLevel: String, childRegion: String, fatherRegion: String): PhMaxDashboardWindowFunc = {
-        val prodLevelRankKeys: List[String] = prodLevel match {
-            case "MOLE" => "PRODUCT_NAME" :: Nil
-            case "PROD" => Nil
-            case _ => logger.error(s"${prodLevel} not implemented."); Nil
-        }
-
-        val regionRankKeys: List[String] = fatherRegion match {
-            case "CITY" => "PROVINCE" :: "CITY" :: Nil
-            case "PROV" => "PROVINCE" :: Nil
-            case "NATION" => Nil
-            case _ => logger.error(s"${prodLevel} not implemented."); Nil
-        }
-
-        val rankWindow: WindowSpec = Window.partitionBy("DATE", "MKT" :: prodLevelRankKeys ::: regionRankKeys: _*)
+    def GenerateShareRowWith(time: String, prodLevel: String, region: String): PhMaxDashboardWindowFunc = {
 
         val newDF: DataFrame = df
-            .withColumn(time + "_" + prodLevel + "_" + childRegion + "_RANK_IN_" + fatherRegion, dense_rank.over(rankWindow.orderBy(col(time + "_" + prodLevel + "_SALES_IN_" + childRegion).desc)))
-            .withColumn(time + "_" + prodLevel + "_" + childRegion + "_SHARE_IN_" + fatherRegion, col(time + "_" + prodLevel + "_SALES_IN_" + childRegion) / col(time + "_" + prodLevel + "_SALES_IN_" + fatherRegion))
+            .withColumn(time + "_" + prodLevel + "_" + region + "_SHARE", when(col(time + "_TOTAL_SALES_IN_" + region) === 0.0, 0.0).otherwise(col(time + "_" + prodLevel + "_SALES_IN_" + region) / col(time + "_TOTAL_SALES_IN_" + region)))
 
         PhMaxDashboardWindowFunc(newDF)
     }
@@ -77,21 +76,21 @@ case class PhMaxDashboardWindowFunc(df: DataFrame) extends PhLogable {
     def GenerateGrowthRowWith(time: String, prodLevel: String, region: String): PhMaxDashboardWindowFunc = {
         val newDF: DataFrame = time match {
             case "MOM" => df
-                .withColumn("MOM_GROWTH_ON_" + prodLevel + "_" + region + "LEVEL", when(col("LAST_M_" + prodLevel + "_SALES_IN_" + region) === 0.0, 0.0).otherwise(col("CURR_" + prodLevel + "_SALES_IN_" + region) - col("LAST_M_" + prodLevel + "_SALES_IN_" + region)))
-                .withColumn("MOM_RATE_ON_" + prodLevel + "_" + region + "LEVEL", when(col("LAST_M_" + prodLevel + "_SALES_IN_" + region) === 0.0, 0.0).otherwise((col("CURR_" + prodLevel + "_SALES_IN_" + region) - col("LAST_M_" + prodLevel + "_SALES_IN_" + region)) / col("LAST_M_" + prodLevel + "_SALES_IN_" + region)))
+                .withColumn("MOM_GROWTH_ON_" + prodLevel + "_" + region, when(col("LAST_M_" + prodLevel + "_SALES_IN_" + region) === 0.0, 0.0).otherwise(col("CURR_" + prodLevel + "_SALES_IN_" + region) - col("LAST_M_" + prodLevel + "_SALES_IN_" + region)))
+                .withColumn("MOM_RATE_ON_" + prodLevel + "_" + region, when(col("LAST_M_" + prodLevel + "_SALES_IN_" + region) === 0.0, 0.0).otherwise((col("CURR_" + prodLevel + "_SALES_IN_" + region) - col("LAST_M_" + prodLevel + "_SALES_IN_" + region)) / col("LAST_M_" + prodLevel + "_SALES_IN_" + region)))
             case "YOY" => df
-                .withColumn("YOY_GROWTH_ON_" + prodLevel + "_" + region + "LEVEL", when(col("LAST_Y_" + prodLevel + "_SALES_IN_" + region) === 0.0, 0.0).otherwise(col("CURR_" + prodLevel + "_SALES_IN_" + region) - col("LAST_Y_" + prodLevel + "_SALES_IN_" + region)))
-                .withColumn("YOY_RATE_ON_" + prodLevel + "_" + region + "LEVEL", when(col("LAST_Y_" + prodLevel + "_SALES_IN_" + region) === 0.0, 0.0).otherwise((col("CURR_" + prodLevel + "_SALES_IN_" + region) - col("LAST_Y_" + prodLevel + "_SALES_IN_" + region)) / col("LAST_M_" + prodLevel + "_SALES_IN_" + region)))
+                .withColumn("YOY_GROWTH_ON_" + prodLevel + "_" + region, when(col("LAST_Y_" + prodLevel + "_SALES_IN_" + region) === 0.0, 0.0).otherwise(col("CURR_" + prodLevel + "_SALES_IN_" + region) - col("LAST_Y_" + prodLevel + "_SALES_IN_" + region)))
+                .withColumn("YOY_RATE_ON_" + prodLevel + "_" + region, when(col("LAST_Y_" + prodLevel + "_SALES_IN_" + region) === 0.0, 0.0).otherwise((col("CURR_" + prodLevel + "_SALES_IN_" + region) - col("LAST_Y_" + prodLevel + "_SALES_IN_" + region)) / col("LAST_M_" + prodLevel + "_SALES_IN_" + region)))
             case _ => logger.error(s"${time} not implemented."); df
         }
 
         PhMaxDashboardWindowFunc(newDF)
     }
 
-    def GenerateEIRowWith(prodLevel: String, childRegion: String, fatherRegion: String): PhMaxDashboardWindowFunc = {
+    def GenerateEIRowWith(prodLevel: String, region: String): PhMaxDashboardWindowFunc = {
 
         val newDF: DataFrame = df
-            .withColumn("EI_" + prodLevel + "_" + childRegion + "_OVER_" + fatherRegion, when(col("LAST_M_" + prodLevel + "_" + childRegion + "_SHARE_IN_" + fatherRegion) === 0.0, 0.0).otherwise(col("LAST_M_" + prodLevel + "_" + childRegion + "_SHARE_IN_" + fatherRegion) / col("CURR_" + prodLevel + "_" + childRegion + "_SHARE_IN_" + fatherRegion)))
+            .withColumn("EI_" + prodLevel + "_" + region, when(col("LAST_M_" + prodLevel + "_" + region + "_SHARE") === 0.0, 0.0).otherwise(col("CURR_" + prodLevel + "_" + region + "_SHARE") / col("LAST_M_" + prodLevel + "_" + region + "_SHARE")))
 
         PhMaxDashboardWindowFunc(newDF)
     }
