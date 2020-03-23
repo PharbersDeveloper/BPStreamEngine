00ea22167de808e3d7e6c123454eb3fd3494c07e
clock <2567729201@qq.com>
2020-03-23 18:32:10 +0800

    准备工作完成，将新旧代码整合并测试通过

--- src/main/scala/com/pharbers/StreamEngine/Jobs/PyJob/PythonJobContainer/BPSPythonJobContainer.scala
+++ src/main/scala/com/pharbers/StreamEngine/Jobs/PyJob/PythonJobContainer/BPSPythonJobContainer.scala
@@ -1,17 +1,21 @@
 package com.pharbers.StreamEngine.Jobs.PyJob.PythonJobContainer
 
-import java.util.{Collections, UUID}
+import java.util.UUID
+import java.util.concurrent.TimeUnit
 import org.mongodb.scala.bson.ObjectId
-import com.pharbers.kafka.schema.DataSet
 import org.apache.spark.sql.SparkSession
-import com.pharbers.StreamEngine.Utils.HDFS.BPSHDFSFile
+import com.pharbers.kafka.schema.{BPJob, HiveTask}
 import com.pharbers.StreamEngine.Jobs.PyJob.BPSPythonJob
+import com.pharbers.kafka.consumer.PharbersKafkaConsumer
+import com.pharbers.kafka.producer.PharbersKafkaProducer
+import org.apache.kafka.clients.consumer.ConsumerRecord
 import com.pharbers.StreamEngine.Utils.Schema.Spark.BPSParseSchema
-import com.pharbers.StreamEngine.Jobs.SandBoxJob.BloodJob.BPSBloodJob
 import com.pharbers.StreamEngine.Utils.Event.EventHandler.BPSEventHandler
 import com.pharbers.StreamEngine.Utils.Event.StreamListener.BPStreamListener
+import com.pharbers.StreamEngine.Utils.GithubHelper.BPSGithubHelper
 import com.pharbers.StreamEngine.Utils.StreamJob.JobStrategy.BPSKfkJobStrategy
 import com.pharbers.StreamEngine.Utils.StreamJob.{BPDynamicStreamJob, BPSJobContainer}
+import com.pharbers.StreamEngine.Utils.ThreadExecutor.ThreadExecutor
 
 object BPSPythonJobContainer {
     def apply(strategy: BPSKfkJobStrategy,
@@ -20,103 +24,159 @@ object BPSPythonJobContainer {
         new BPSPythonJobContainer(spark, config)
 }
 
-/** 执行 Python 的 Job
+/** 执行 Python 清洗的 Container
  *
  * @author clock
  * @version 0.0.1
  * @since 2019/11/6 17:43
- * @node 可用的配置参数
+ * @node config 可用的配置参数
  * {{{
- *      jobId = UUID // 默认
- *      metadataPath = "/user/clock/jobs/metadataPath/$lastJobID"
- *      filesPath = "/user/clock/jobs/filesPath/$lastJobID"
- *      resultPath = "/user/clock/jobs/resultPath" // 后面会自动加上当前的 jobId
- *      partition = "4" // 默认4
+ *      containerId = UUID // 缺省 UUID
  *
- *      parentsOId = "List(parent1, parent2)" // 默认 Nil
- *      mongoOId = "oid" // 默认 new ObjectId().toString
+ *      listenerTopic = “PyJobContainerListenerTopic” // JobContainer 监听的 topic
+ *      defaultNoticeTopic = “PyJobContainerNoticeTopic” // Job 任务完成通知的`默认` topic, 如果单个 Job 有 Topic， 以 Job 的为准
+ *
+ *      defaultPartition = "4" // 默认4，表示每个 Job 可使用的 spark 分区数，也是可用 Python 的线程数，默认 4 线程
+ *      defaultRetryCount = "3" // 默认, 重试次数
+ *
+ *      pythonUri = "" // python 清洗代码的 github 路径
+ *      pythonBranch = "" // python 清洗代码的分支
  * }}}
  */
 class BPSPythonJobContainer(override val spark: SparkSession,
                             config: Map[String, String])
         extends BPSJobContainer with BPDynamicStreamJob with Serializable {
 
-    override val strategy: BPSKfkJobStrategy = null
     type T = BPSKfkJobStrategy
+    override val strategy: BPSKfkJobStrategy = null
 
-    val id: String = config.getOrElse("jobId", UUID.randomUUID().toString)
-    val metadataPath: String = config("metadataPath")
-    val filesPath: String = config("filesPath")
-    val resultPath: String = config.getOrElse("resultPath", "./jobs/")
-    val partition: String = config.getOrElse("partition", "4")
+    override val id: String = config.getOrElse("containerId", UUID.randomUUID().toString)
+    val containerId: String = id
 
-    val parentsOId: List[CharSequence] =
-        config.getOrElse("parentsOId", "").split(",").toList.map(_.asInstanceOf[CharSequence])
-    val mongoOId: String = config.getOrElse("mongoOId", new ObjectId().toString)
+    val listenerTopic: String = config.getOrElse("listenerTopic", "PyJobContainerListenerTopic")
+    val defaultNoticeTopic: String = config.getOrElse("noticeTopic", "PyJobContainerNoticeTopic")
 
-    var metadata: Map[String, Any] = Map.empty
-    val pyFiles = List(
-        "./pyClean/main.py",
-        "./pyClean/results.py",
-        "./pyClean/auth.py",
-        "./pyClean/mapping.py",
-        "./pyClean/cleaning.py"
-    )
+    val defaultPartition: String = config.getOrElse("defaultPartition", "4")
+    val defaultRetryCount: String = config.getOrElse("defaultRetryCount", "3")
 
-    // 当所需文件未准备完毕，则等待
-    def notFoundShouldWait(path: String): Unit = {
-        if (!BPSHDFSFile.checkPath(path)) {
-            logger.debug(path + "文件不存在，等待 1s")
-            Thread.sleep(1000)
-            notFoundShouldWait(path)
-        }
+    val pythonUri: String = config("pythonUri")
+    val pythonBranch: String = config.getOrElse("pythonBranch", "master")
+
+    // 将 python 清洗文件发送到 spark
+    def sendPy2Spark(): Unit = {
+        val helper: BPSGithubHelper = BPSGithubHelper()
+        helper.cloneByBranch(containerId, pythonUri, pythonBranch)
+        val pyFiles: List[String] = helper.listFile(containerId, ".py")
+        pyFiles.foreach(spark.sparkContext.addFile)
     }
 
-    override def open(): Unit = {
-        notFoundShouldWait(metadataPath)
-//        notFoundShouldWait(filesPath)
-        logger.info(s"执行Python Container")
-        metadata = BPSParseSchema.parseMetadata(metadataPath)(spark)
+    import org.json4s._
+    import org.json4s.jackson.JsonMethods._
+
+    /** kafka consumer 收到消息的 function
+     *
+     * @author clock
+     * @version 0.0.1
+     * @since 2020/3/23 14:08
+     * @node config 可用的配置参数
+     * {{{
+     *      jobId = UUID // 缺省 UUID
+     *      parentsId = ObjectId // 缺省 ""
+     *      datasetId = ObjectId  // 缺省 new ObjectId().toString
+     *
+     *      noticeTopic = defaultNoticeTopic // Job 任务完成通知topic, 如果单个 Job 有 Topic， 以 Job 的为准，如果 Job 没用，使用默认 Topic
+     *      partition = defaultPartition //默认 “4”，表示每个 Job 可使用的 spark 分区数，也是可用 Python 的线程数，默认 4 线程
+     *      retryCount = defaultRetryCount // 默认, 重试次数
+     *
+     *      metadataPath = "" // 流数据的元信息
+     *      filesPath = "" // 流数据存放位置
+     *      resultPath = "./jobs/" // 默认"./jobs/"，Job 执行后的结果的存放位置, 会自动添加 containerId
+     * }}}
+     */
+    implicit val formats: DefaultFormats.type = org.json4s.DefaultFormats
+    val consumerFunc: ConsumerRecord[String, BPJob] => Unit = { record: ConsumerRecord[String, BPJob] =>
+        val jobMsg = parse(record.value().getJob.toString).extract[Map[String, Any]]
+
+        // 获得 PyJob 参数信息
+        val jobId: String = jobMsg.getOrElse("jobId", UUID.randomUUID()).toString
+        val parentsId: List[CharSequence] = jobMsg.getOrElse("parentsId", "").toString.split(",").toList.map(_.asInstanceOf[CharSequence])
+        val datasetId: String = jobMsg.getOrElse("datasetId", new ObjectId()).toString
+
+        val noticeTopic: String = jobMsg.getOrElse("noticeTopic", defaultNoticeTopic).toString
+        val partition: String = jobMsg.getOrElse("partition", defaultPartition).toString
+        val retryCount: String = jobMsg.getOrElse("retryCount", defaultRetryCount).toString
+
+        val metadataPath: String = jobMsg("metadataPath").toString
+        val filesPath: String = jobMsg("filesPath").toString
+        val resultPath: String = {
+            val path = jobMsg.getOrElse("resultPath", "./jobs/").toString
+            if (path.endsWith("/")) path + containerId
+            else path + "/" + containerId
+        }
+
+        // 解析 metadata
+        val metadata = BPSParseSchema.parseMetadata(metadataPath)(spark)
         val loadSchema = BPSParseSchema.parseSchema(metadata("schema").asInstanceOf[List[_]])
 
+        // 读取输入流
         val reading = spark.readStream
                 .schema(loadSchema)
                 .option("startingOffsets", "earliest")
-                //todo: 设置触发的文件数，以控制内存 效果待测试
-                .option("maxFilesPerTrigger", 4)
+                //TODO: 设置触发的文件数，以控制内存 效果待测试
+                .option("maxFilesPerTrigger", partition.toInt)
                 .parquet(filesPath)
 
-        inputStream = Some(reading)
+        // 真正执行 Job
+        val job = BPSPythonJob(jobId, spark, Some(reading), noticeFunc, finishJobWithId, Map(
+            "noticeTopic" -> noticeTopic,
+            "datasetId" -> datasetId,
+            "parentsId" -> parentsId,
+            "resultPath" -> resultPath,
+            "lastMetadata" -> metadata,
+            "fileSuffix" -> "csv",
+            "partition" -> partition,
+            "retryCount" -> retryCount
+        ))
 
-        // 注册血统
-        import collection.JavaConverters._
-        val dfs = new DataSet(
-            parentsOId.asJava,
-            mongoOId,
-            id,
-            Collections.emptyList(),
-            "",
-            metadata("length").asInstanceOf[Double].toLong,
-            s"$resultPath/$id/contents",
-            "Python 清洗 Job")
-        BPSBloodJob("data_set_job", dfs).exec()
+        job.open()
+        job.exec()
     }
 
-    override def exec(): Unit = inputStream match {
-        case Some(_) =>
-            //todo: 为了submit后能使用，时使用--file预先加入了file。之后可以选择将py文件放在hdfs中，这儿根据配置的hdfs目录加载
-            // 目前是临时办法，打算使用 github 存放 python 脚本, 每次坚持指定分支是否有更新，然后下载并发送到 Spark
-	        // TODO: 本地调试打开
-//            pyFiles.foreach(spark.sparkContext.addFile)
-            val job = BPSPythonJob(id, spark, inputStream, this, Map(
-                "resultPath" -> resultPath,
-                "lastMetadata" -> metadata,
-                "partition" -> partition,
-                "mongoId" -> mongoOId
-            ))
-            job.open()
-            job.exec()
-        case None => ???
+    val noticeFunc: (String, Map[String, Any]) => Unit = { (noticeTopic: String, noticeMap: Map[String, Any]) =>
+        val jobId = noticeMap("jobId").toString
+        val length = noticeMap("length").asInstanceOf[Long]
+        val datasetId = noticeMap("datasetId").toString
+        val successPath = noticeMap("successPath").toString
+
+        val pkp = new PharbersKafkaProducer[String, HiveTask]
+        val msg = new HiveTask(jobId, "", datasetId, "append", successPath, length.toInt, "")
+        val end = new HiveTask(jobId, "", "", "end", "", 0, "")
+        List(msg, end).foreach(x => {
+            val fu = pkp.produce(noticeTopic, "", x)
+            logger.info(fu.get(10, TimeUnit.SECONDS))
+        })
+        pkp.producer.close()
+    }
+
+    // open kafka consumer
+    var pyConsumer: Option[PharbersKafkaConsumer[String, BPJob]] = None
+
+    override def open(): Unit = {
+        sendPy2Spark()
+        if (pyConsumer.isEmpty) {
+            logger.info(s"open kafka consumer, listener topic is `$listenerTopic`")
+            val pkc = new PharbersKafkaConsumer(topics = List(listenerTopic), process = consumerFunc)
+            ThreadExecutor().execute(pkc)
+            pyConsumer = Some(pkc)
+        } else {
+            logger.info(s"kafka consumer is opened")
+        }
+    }
+
+    override def close(): Unit = {
+        pyConsumer.get.close()
+        BPSGithubHelper().delDir(id)
+        super.close()
     }
 
     override def registerListeners(listener: BPStreamListener): Unit = {}
--- src/test/scala/com/pharbers/StreamEngine/Others/clock/ReadParquetScript.scala
+++ src/test/scala/com/pharbers/StreamEngine/Others/clock/ReadParquetScript.scala
@@ -1,7 +1,9 @@
 package com.pharbers.StreamEngine.Others.clock
 
+import org.scalatest.FunSuite
 import com.pharbers.StreamEngine.Utils.Schema.Spark.BPSParseSchema
 import com.pharbers.StreamEngine.Utils.Session.Spark.BPSparkSession
+import org.apache.spark.sql.SparkSession
 
 /** 查看Parquet的脚本
  *
@@ -10,56 +12,50 @@ import com.pharbers.StreamEngine.Utils.Session.Spark.BPSparkSession
  * @since 2019/11/6 15:08
  * @note
  */
-object ReadParquetScript extends App {
-    val runId = "83ee0f2a-360a-4236-ba26-afa09d58e01d"
-    val jobId = "ea293f1b-a66d-44fb-95ff-49a009840ed4"
-//    val matedataPath = s"/jobs/$runId/$jobId/metadata"
-//    val filesPath = s"/jobs/$runId/$jobId/contents/$jobId"
+class ReadParquetScript extends FunSuite {
+    val spark: SparkSession = BPSparkSession()
 
-    val spark = BPSparkSession()
-    val id = "08d72f26-f5cc-4618-911c-812a4a4e1cec"
-    def byBatchForCsv(): Unit = {
-        val jobId = "0090df5d-8858-46e3-a4fa-ce53e183e905"
-
-//        val path = s"hdfs:///user/clock/jobs/$jobId/metadata"
-        val path = s"hdfs:///user/clock/jobs/$jobId/contents"
-//        val path = s"hdfs:///user/clock/jobs/$jobId/err"
-//        val path = s"hdfs:///user/clock/jobs/$jobId/row_record"
+    test("read csv") {
+        val path = s"/user/clock/jobs/jobId/contents"
 
         val reading = spark.read
                 .format("com.databricks.spark.csv")
-//                .option("header", "true") //这里如果在csv第一行有属性的话，没有就是"false"
-                .option("inferSchema", true.toString)//这是自动推断属性列的数据类型。
+//                .option("header", "true")     //这里如果在csv第一行有属性的话，没有就是"false"
+                .option("inferSchema", true.toString)   //这是自动推断属性列的数据类型。
                 .load(path) //文件的路径
+
         reading.show(false)
         println(reading.count())
     }
-//    byBatchForCsv()
 
-    val testPath = "hdfs://jobs/01e951cb-34ed-4644-9a9e-fc3f570e0613/c65c732e-fec8-41ea-b891-ef737837a354/contents"
-    val metadataPath = "hdfs://jobs/01e951cb-34ed-4644-9a9e-fc3f570e0613/c65c732e-fec8-41ea-b891-ef737837a354/metadata"
-    def byBatch(): Unit = {
+    test("read parquet use batch") {
+        val metadataPath = "/jobs/02c07385-39fa-496a-a9ac-029ed09aa79c/0db59660-056f-4d48-9ade-90b8ceaadc57/metadata"
+        val filePath = "/jobs/02c07385-39fa-496a-a9ac-029ed09aa79c/0db59660-056f-4d48-9ade-90b8ceaadc57/contents"
+
         val loadSchema = BPSParseSchema.parseSchemaByMetadata(metadataPath)(spark)
-        val reading = spark.read.schema(loadSchema).parquet(testPath)
+        val reading = spark.read.schema(loadSchema).parquet(filePath)
+
         reading.show(false)
         println(reading.count())
     }
-    byBatch()
 
-//    def byStream(): Unit = {
-//        val loadSchema = BPSParseSchema.parseSchemaByMetadata(matedataPath)(spark)
-//        val reading = spark.readStream
-//                .schema(loadSchema)
-//                .option("startingOffsets", "earliest")
-//                .parquet(filesPath)
-//
-//        val wordCounts = reading.groupBy("1#Year").count()
-//
-//        val query = wordCounts.writeStream
-//                .outputMode("complete")
-//                .format("console")
-//                .start()
-//        query.awaitTermination()
-//    }
-//    byStream()
+    // FIX 测试未通过
+    test("read parquet use stream") {
+        val metadataPath = "/jobs/02c07385-39fa-496a-a9ac-029ed09aa79c/0db59660-056f-4d48-9ade-90b8ceaadc57/metadata"
+        val filePath = "/jobs/02c07385-39fa-496a-a9ac-029ed09aa79c/0db59660-056f-4d48-9ade-90b8ceaadc57/contents"
+
+        val loadSchema = BPSParseSchema.parseSchemaByMetadata(metadataPath)(spark)
+        val reading = spark.readStream
+                .schema(loadSchema)
+                .option("startingOffsets", "earliest")
+                .parquet(filePath)
+
+        val wordCounts = reading.groupBy("2#年").count()
+
+        val query = wordCounts.writeStream
+                .outputMode("complete")
+                .format("console")
+                .start()
+        query.awaitTermination()
+    }
 }
