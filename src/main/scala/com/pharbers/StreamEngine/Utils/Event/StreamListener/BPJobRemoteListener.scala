81dd18da3c08e78e7f0304b71c5a6991600d95fd
dcs <419787760@qq.com>
2020-04-08 15:46:54 +0800

    BPJobRemoteListener active和deActive反了

--- src/main/scala/com/pharbers/StreamEngine/Utils/Event/StreamListener/BPJobRemoteListener.scala
+++ src/main/scala/com/pharbers/StreamEngine/Utils/Event/StreamListener/BPJobRemoteListener.scala
@@ -4,12 +4,10 @@ import com.pharbers.StreamEngine.Utils.Event.{BPSEvents, BPSTypeEvents}
 import com.pharbers.StreamEngine.Utils.Job.BPStreamJob
 import org.apache.spark.sql.DataFrame
 
-import scala.reflect.ClassTag
-
 /** 功能描述
   *
   * @param args 构造参数
-  * @tparam T 构造泛型参数 case class，scala集合类，scala基本类
+  * @tparam T 构造泛型参数 case class，scala集合类，scala基本类，case class中在json中可能没有的属性需要使用option.属性默认值无效
   * @author dcs
   * @version 0.0
   * @since 2020/04/07 15:58
@@ -17,18 +15,18 @@ import scala.reflect.ClassTag
   */
 class BPJobRemoteListener[T](override val job: BPStreamJob, hitTypes: List[String])(trigFunc: BPSTypeEvents[T] => Unit) extends BPStreamRemoteListener{
 
-    override def hit(e: BPSEvents): Boolean = hitTypes.contains(e.`type`)
+    override def hit(e: BPSEvents): Boolean = e != null && hitTypes.contains(e.`type`)
 
     override def trigger(e: BPSEvents): Unit = {
         trigFunc(BPSTypeEvents[T](e))
     }
 
     override def active(s: DataFrame): Unit = {
-        BPSDriverChannel.unRegisterListener(this)
+        BPSDriverChannel.registerListener(this)
     }
 
     override def deActive(): Unit = {
-        BPSDriverChannel.registerListener(this)
+        BPSDriverChannel.unRegisterListener(this)
     }
 }
 
