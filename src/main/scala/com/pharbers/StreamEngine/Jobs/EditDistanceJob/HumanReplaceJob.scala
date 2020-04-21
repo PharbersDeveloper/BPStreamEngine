f1556b6873ca6c8d2f4540352ee3cd6f04ba3013
dcs <419787760@qq.com>
2020-04-21 14:01:21 +0800

    新增人工处理表job，并和之前的数据兼容

--- src/main/scala/com/pharbers/StreamEngine/Jobs/EditDistanceJob/HumanReplaceJob.scala
+++ src/main/scala/com/pharbers/StreamEngine/Jobs/EditDistanceJob/HumanReplaceJob.scala
@@ -7,7 +7,7 @@ import com.pharbers.StreamEngine.Utils.Strategy.JobStrategy.BPSCommonJobStrategy
 import com.pharbers.StreamEngine.Utils.Strategy.Session.Spark.BPSparkSession
 import org.apache.kafka.common.config.ConfigDef
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{Column, DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
 
 /** 功能描述
@@ -43,6 +43,7 @@ class HumanReplaceJob(override val componentProperty: Component2.BPComponentConf
     override def exec(): Unit = {
     }
 
+    //针对No_Replace_HBV 0228-Final
     def createHumanReplaceDf(df: DataFrame): Unit = {
         //todo:配置传入
         val tableVersion = getVersion(tableName)
@@ -64,6 +65,7 @@ class HumanReplaceJob(override val componentProperty: Component2.BPComponentConf
 
     }
 
+    //针对CPA_no_replace 第8次提数0417之前的
     def createHumanReplaceDfV2(df: DataFrame): Unit = {
         //todo:配置传入
         val mode = "append"
@@ -98,20 +100,86 @@ class HumanReplaceJob(override val componentProperty: Component2.BPComponentConf
 
     }
 
+    def createHumanReplaceDfV3(df: DataFrame): Unit = {
+        val minColumns = List("MOLE_NAME", "PRODUCT_NAME", "SPEC", "DOSAGE", "PACK_QTY", "MANUFACTURER_NAME")
+
+        val minDf = df.na.fill("")
+                .selectExpr(List("ORIGIN as ORI", "CANDIDATE as CAN", "COL_NAME as NAME") ::: minColumns.map(x => s"ORIGIN_$x as $x"): _*)
+                .withColumn("CAN", regexp_replace(col("CAN"), "[\\[\\]\"]", ""))
+                .withColumn("min", concat(minColumns.map(x => col(x)): _*))
+                .withColumn("columns", map())
+
+        val humanDf = df.na.fill("na", Seq("REMARK")).filter("REMARK != 'na'")
+                .withColumn("CANDIDATE", regexp_replace(col("CANDIDATE"), "[\\[\\]\"]", ""))
+
+        val rules = List("ORIGIN=CANDIDATE",
+                "ORIGIN+ORIGIN_MOLE_NAME+ORIGIN_PRODUCT_NAME+ORIGIN_DOSAGE=CANDIDATE",
+                "ORIGIN+ORIGIN_MOLE_NAME+ORIGIN_DOSAGE+ORIGIN_MANUFACTURER_NAME=CANDIDATE",
+                "ORIGIN+ORIGIN_MOLE_NAME+ORIGIN_PRODUCT_NAME=CANDIDATE",
+                "ORIGIN+ORIGIN_PRODUCT_NAME=CANDIDATE",
+                "ORIGIN+ORIGIN_MOLE_NAME+ORIGIN_PRODUCT_NAME+ORIGIN_MANUFACTURER_NAME=CANDIDATE",
+                "ORIGIN+ORIGIN_MOLE_NAME=CANDIDATE",
+                "ORIGIN+ORIGIN_MOLE_NAME+ORIGIN_SPEC+ORIGIN_DOSAGE+ORIGIN_MANUFACTURER_NAME=CANDIDATE",
+                "ORIGIN+ORIGIN_MOLE_NAME+ORIGIN_MANUFACTURER_NAME=CANDIDATE",
+                "ORIGIN+ORIGIN_MOLE_NAME+ORIGIN_DOSAGE=CANDIDATE",
+                "ORIGIN_MOLE_NAME+ORIGIN_PRODUCT_NAME=CANDIDATE",
+                "ORIGIN_PRODUCT_NAME=CANDIDATE")
+        val res = rules.foldLeft(minDf)((df, rule) => joinWithHumanRule(df, humanDf.filter($"REMARK" === rule), rule))
+        res.write.parquet("/user/dcs/test/human_replace")
+
+        val humanReplaceDf = spark.read.parquet("/user/dcs/test/human_replace")
+        val tableVersion = getVersion(tableName)
+        val oldTable = spark.read.parquet(s"/common/public/human_replace/$tableVersion")
+                .withColumn("cols", map(minColumns.flatMap(x => List(lit(x), col(x))): _*))
+                .selectExpr("min as oldMin", "cols")
+
+        val margeMap = udf((map1: Map[String, String], map2: Map[String, String]) => map1 ++ map2.filter(x => x._2 != ""))
+        val humanReplaceTable = humanReplaceDf.filter(size($"columns") > 0)
+                .select("min", "columns")
+                .groupBy("min").agg(first("columns") as "columns")
+                .join(oldTable, $"min" === $"oldMin", "full")
+                .withColumn("min", when($"min".isNull, $"oldMin") otherwise $"min")
+                .withColumn("columns", when($"columns".isNull, $"cols") otherwise $"columns")
+                .withColumn("columns", when($"cols".isNotNull, margeMap($"columns", $"cols")) otherwise $"columns")
+                .selectExpr("min" +: minColumns.map(x => s"columns['$x'] as $x"): _*)
+        val mode = "overwrite"
+        val version = getVersion(tableName, mode)
+        humanReplaceTable.withColumn("version", lit(version)).write
+                .option("path", s"/common/public/human_replace/$version")
+                .mode(mode)
+                .saveAsTable(tableName)
+    }
+
+    def joinWithHumanRule(minDf: DataFrame, humanDf: DataFrame, rule: String): DataFrame ={
+        val join: String => Column = s => s.split("=").head.split("\\+")
+                .map(x => if(x == "ORIGIN") $"ORIGIN" === $"ORI" and $"CANDIDATE" === $"CAN" else col(x) === col(x.replace("ORIGIN_", "")))
+                .foldLeft($"NAME" === $"COL_NAME")((l, r) => l and r)
+
+        val selectCols = rule.split("=").head.split("\\+")
+                .flatMap(x => if(x == "ORIGIN") List("ORIGIN", "CANDIDATE") else List(x))
+        val humanDfFilter = humanDf.select("COL_NAME", selectCols: _*).distinct()
+        val addMap= udf((map1: Map[String, String], key: String, value: String) => map1 ++ Map(key -> value))
+        val res = minDf.join(humanDfFilter, join(rule), "left")
+                .withColumn("columns", when($"COL_NAME".isNotNull, addMap($"columns", $"COL_NAME", $"CAN")) otherwise $"columns")
+                .select(minDf.columns.map(x => col(x)): _*)
+        res
+    }
+
 }
 
 case class HumanReplaceRow(min: String, col_name: String, origin: String, cols: Map[String, String])
 
 object TestHumanReplaceJob extends App {
 
-    val localSpark = BPSConcertEntry.queryComponentWithId("spark").get.asInstanceOf[BPSparkSession]
-
+    val localSpark = SparkSession.builder().config(new SparkConf().setMaster("local[8]")).enableHiveSupport().getOrCreate()
+    localSpark.sparkContext.setLogLevel("INFO")
     val job = HumanReplaceJob(BPSComponentConfig("", "", Nil, Map()))
 
     val df = localSpark.read.format("csv")
             .option("header", "true")
-            .load("file:///D:\\文件\\excel数据表\\产品\\humanReplace\\CPA_no_replace 第五次提数0413.csv")
-    job.createHumanReplaceDfV2(df)
+            .load("file:///D:\\文件\\excel数据表\\产品\\humanReplace\\CPA_no_replace 第8次提数0417.csv")
+    df.show()
+//    job.createHumanReplaceDfV3(df)
     println("over")
     spark.close()
 }
