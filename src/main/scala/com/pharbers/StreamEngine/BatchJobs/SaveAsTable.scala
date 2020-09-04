6d4bf3dd08706eb58a1fab956708e042c42f1ac4
clock <2567729201@qq.com>
2020-09-04 16:48:28 +0800

    之前的注释不好拷贝

--- src/main/scala/com/pharbers/StreamEngine/BatchJobs/SaveAsTable.scala
+++ src/main/scala/com/pharbers/StreamEngine/BatchJobs/SaveAsTable.scala
@@ -9,22 +9,22 @@ import org.apache.spark.sql.SparkSession
  * @since 2020/09/04 15:32
  * @note
  * {{{
- * $SPARK_HOME/bin/spark-submit \
- * --name saveAsTable-submit \
- * --master yarn \
- * --deploy-mode cluster \
- * --driver-memory 1g \
- * --executor-memory 1g \
- * --executor-cores 1 \
- * --num-executors 1 \
- * --conf spark.hadoop.fs.s3a.access.key=AKIAWPBDTVEAJ6CCFVCP \
- * --conf spark.hadoop.fs.s3a.secret.key=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 \
- * --conf spark.hadoop.fs.s3a.endpoint=s3.cn-northwest-1.amazonaws.com.cn \
- * --class com.pharbers.StreamEngine.BatchJobs.SaveAsTable \
- * s3a://ph-platform/2020-08-10/functions/scala/BPStream/SaveAsTable-20200904.jar \
- * s3a://ph-stream/common/public/prod/16 \
- * s3a://ph-stream/common/public/prod/17 \
- * prod20
+$SPARK_HOME/bin/spark-submit \
+--name saveAsTable-submit \
+--master yarn \
+--deploy-mode cluster \
+--driver-memory 1g \
+--executor-memory 1g \
+--executor-cores 1 \
+--num-executors 1 \
+--conf spark.hadoop.fs.s3a.access.key=AKIAWPBDTVEAJ6CCFVCP \
+--conf spark.hadoop.fs.s3a.secret.key=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 \
+--conf spark.hadoop.fs.s3a.endpoint=s3.cn-northwest-1.amazonaws.com.cn \
+--class com.pharbers.StreamEngine.BatchJobs.SaveAsTable \
+s3a://ph-platform/2020-08-10/functions/scala/BPStream/SaveAsTable-20200904.jar \
+s3a://ph-stream/common/public/prod/16 \
+s3a://ph-stream/common/public/prod/17 \
+prod20
  * }}}
  */
 object SaveAsTable {
